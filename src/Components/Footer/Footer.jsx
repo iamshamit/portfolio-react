a08e6626254a85e2f38d13c2a789f03f896ce61f
Shamit Mishra <82385464+iamshamit@users.noreply.github.com>
2024-06-12 17:46:40 +0530

    small changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,26 +6,28 @@ import user_icon from '../../assets/user_icon.svg'
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
-        <div className="footer-top">
-            <div className="footer-top-left">
-                <img src={footer_logo} alt="" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nesciunt quas veniam aspernatur vel magnam dolore quibusdam enim! Quos, dolorum.</p>
-            </div>
-            <div className="footer-top-right">
-                <div className="footer-email-input">
-                    <img src={user_icon} alt="" />
-                    <input type="email" placeholder='Enter your email' />
+        {/* 
+            <div className="footer-top">
+                <div className="footer-top-left">
+                    <img src={footer_logo} alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nesciunt quas veniam aspernatur vel magnam dolore quibusdam enim! Quos, dolorum.</p>
                 </div>
-                <div className="footer-subscribe">Subscribe</div>
+                <div className="footer-top-right">
+                    <div className="footer-email-input">
+                        <img src={user_icon} alt="" />
+                        <input type="email" placeholder='Enter your email' />
+                    </div>
+                    <div className="footer-subscribe">Subscribe</div>
+                </div>
             </div>
-        </div>
+        */}
         <hr />
         <div className="footer-bottom">
             <p className="footer-bottom-left">© 2024 Shamit Mishra.  All Rights</p>
             <div className="footer-bottom-right">
                 <p>Term of Services</p>
                 <p>Privacy policy</p>
-                <p>Connect with me</p>
+                <a target="_blank" href='https://www.linkedin.com/in/shamitmishra/' style={{ textDecoration: 'none', color: 'white' }}><p style={{ color: 'white', textDecoration: 'none' }}>Connect with me</p></a>
             </div>
         </div>
     </div>
